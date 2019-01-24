1ce38a331ba2ec992e65dcdd656adb792df5cc49
Carl Vitullo <carl.vitullo@gmail.com>
2019-01-23 22:20:18 -0500

    Add basic text components

---
+++ src/basics/Text.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Article = styled.article``;
+export const Text = styled.span``;
+export const Paragraph = styled.p``;
+export const TextLink = styled.a``;
+export const Image = styled.img``;
+
+export const Break = styled.hr``;
+
+export const Title = styled.h1``;
+export const Heading = styled.h2``;
+export const Subheading = styled.h3``;
+export const H4 = styled.h4``;
+export const H5 = styled.h5``;
+export const H6 = styled.h6``;
